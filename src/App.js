a9c356b6683fc0745335b81041fe971d561dcf11
Caswell Orr <caswell.orr@gmail.com>
2022-01-24 13:21:33 -0600

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useState} from "react";
+
 function App() {
+  
+  const [toDos, setToDos] = useState(["Do Dishes", "Do laundry"]);
+  const [newToDo, setNewToDo] = useState("");
+
+  function addToDo() {
+
+    setToDos([newToDo, ...toDos]);
+
+  };
+ 
+  function deleteToDo(item) {
+     console.log(item);
+
+    const newList = toDos.filter((todo) => {
+      if(todo === item) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    setToDos(newList);
+
+  };
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {toDos.map((toDo, index) => 
+        (
+        <div 
+          key={index}
+          onClick={() => deleteToDo(toDo)}>
+            {toDo}
+          </div>
+          )
+        )}
+
+      <form>
+        <input type="text" value={newToDo} onChange={(event)=> setNewToDo(event.target.value)}></input>
+
+        <button onSubmit={(event)=> {
+          event.preventDefault();
+          addToDo(event)
+        }}>Add To Do</button>
+        
+      </form>
+      
     </div>
   );
 }
